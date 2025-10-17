e9738fe76eecc10809e21e1316fd4634062888c3
eliot-hash <eliot@beton.win>
2025-10-17 14:19:07 +0300

    normalize error message

--- pwa-event-handler/src/common/interceptors/session.interceptor.ts
+++ pwa-event-handler/src/common/interceptors/session.interceptor.ts
@@ -1,6 +1,7 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import {EventHandlerRepository} from "../../core/event-handler.repository";
+import { status } from '@grpc/grpc-js';
 
 type BodyWithSession = { sessionId?: string; userId?: string };
 
@@ -11,12 +12,18 @@ export class SessionExistsPipe implements PipeTransform<BodyWithSession, any> {
     async transform(value: BodyWithSession) {
         const { sessionId } = value ?? {};
         if (!sessionId) {
-            throw new RpcException('sessionId is required');
+            throw new RpcException({
+                code: status.INVALID_ARGUMENT,
+                message: 'sessionId is required'
+            });
         }
 
         const session = await this.repo.getSessionById(sessionId);
         if (!session) {
-            throw new RpcException('Session not found');
+            throw new RpcException({
+                code: status.NOT_FOUND,
+                message: 'Session not found'
+            });
         }
 
         return Object.assign(value);
