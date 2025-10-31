5f22acf4d5f08a93011555e05c65929444992ffb
eliot-hash <eliot@beton.win>
2025-10-31 16:17:47 +0200

    vite update

--- pwa_vite/src/axios/instance.ts
+++ pwa_vite/src/axios/instance.ts
@@ -1,6 +1,6 @@
 import axios, { type AxiosInstance } from "axios";
 
-const BASE_URL = import.meta.env.API_BASE_URL || "http://72.60.247.106:3000";
+const BASE_URL = import.meta.env.VITE_APP_API_BASE_URL || "http://72.60.247.106:3000";
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
