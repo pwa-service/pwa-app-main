54256e1b59098bf60455181ca94abf7b16ac65bf
eliot-hash <eliot@beton.win>
2025-11-07 04:55:27 +0200

    fix build

--- pwa-builder/src/builds/pwa_vite_test-pwa-42.local.com/src/context/pwa-install/PWAInstallContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import type { usePWAInstall } from "../../hooks/usePWAInstall";
-
-export const PWAInstallContext = createContext<ReturnType<typeof usePWAInstall> | null>(null);
--- pwa-builder/src/builds/pwa_vite_test-pwa-42.local.com/src/context/tracker/TrackerContext.tsx
+++
@@ -1,5 +0,0 @@
-import type { UseTrackerStoreReturn } from "../../store/useTrackingStore";
-
-import { createContext } from "react";
-
-export const TrackerContext = createContext<UseTrackerStoreReturn | null>(null);
