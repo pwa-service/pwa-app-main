1a416033761e3ad32b3066543951c61e220b2e46
developer-lumen <lumen@beton.win>
2025-11-21 18:05:34 +0200

    filled new pwa with app content (QUINI CASINO)

---
+++ pwa_vite/src/constants/quini-casino/description.ts
@@ -0,0 +1,5 @@
+export const description: string[] = [
+  "¡Quini6 Casino, la app oficial para los fanáticos del juego en Argentina! 🇦🇷Donde te regalamos un BONO de 700% + 600 Giros Gratis para que empieces a jugar a lo grande. 🎰✨",
+  "🔥 ¡Lo mejor es que recibís tus premios rapidísimo! Es fácil, rápido y miles de argentinos ya están jugando y ganando todos los días. 🚀",
+  "Solo tenés que descargar la app, registrarte, hacer tu primer depósito ¡y comenzar la diversión! 🌟 La suerte puede estar de tu lado en el próximo sorteo — ¿listo para ganar en grande? 💎",
+];
