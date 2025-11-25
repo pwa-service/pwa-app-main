740f25e2350debad7852aa6c4bb95702cda18e2e
developer-lumen <lumen@beton.win>
2025-11-25 15:52:21 +0200

    removed useSmartLook log

--- pwa_vite/src/hooks/useSmartLook.tsx
+++ pwa_vite/src/hooks/useSmartLook.tsx
@@ -1,18 +1,10 @@
 import { useEffect } from "react";
 import Smartlook from "smartlook-client";
 
-const env = import.meta.env;
-
-export function useSmartLook() {
-  const appID = env.VITE_SMARTLOOK_APP_ID;
+export const useSmartLook = () => {
+  const appID = import.meta.env.VITE_SMARTLOOK_APP_ID;
 
   useEffect(() => {
-    if (typeof window !== "undefined" && appID) {
-      if (window.location.pathname === "/") {
-        console.log("Smartlook enabled for /");
-        console.log(appID);
-        Smartlook.init(appID);
-      }
-    }
+    if (typeof window !== "undefined" && appID) Smartlook.init(appID);
   }, [appID]);
-}
+};
