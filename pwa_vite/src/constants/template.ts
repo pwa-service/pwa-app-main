811c9fd4bcbfac9442156a95721983fa57ee9227
developer-lumen <lumen@beton.win>
2025-12-03 12:48:02 +0200

    created lava casino pwa

--- pwa_vite/src/constants/template.ts
+++ pwa_vite/src/constants/template.ts
@@ -4,11 +4,11 @@ import sliderImage1 from "../assets/slider_image_1.webp";
 import sliderImage2 from "../assets/slider_image_2.webp";
 import sliderImage3 from "../assets/slider_image_3.webp";
 import sliderImage4 from "../assets/slider_image_4.webp";
-import sliderImage5 from "../assets/slider_image_5.webp";
+// import sliderImage5 from "../assets/slider_image_5.webp";
 
 export const data = {
   productImage,
-  productName: "Kino Casino",
+  productName: "Agüero Casino",
   productCreator: "BetonWin",
 
   images: [
@@ -16,14 +16,16 @@ export const data = {
     { src: sliderImage2, alt: "image 2" },
     { src: sliderImage3, alt: "image 3" },
     { src: sliderImage4, alt: "image 4" },
-    { src: sliderImage5, alt: "image 5" },
+    // { src: sliderImage5, alt: "image 5" },
   ],
 
   description: [
-    "🎰 ¡Bienvenido a Kino Casino – la nueva app oficial de Lotería de Chile!",
-    "🇨🇱 ¿Eres chileno? Entonces esto es para ti: recibe hasta 💰$5.000.000 CLP y 600 giros gratis solo por registrarte.",
-    "🔥 Juega los slots más populares, elegidos por jugadores como tú.",
-    "🎯 Con Kino Lotería, ganar nunca fue tan fácil.",
-    "📲 ¡Descárgala ahora y empieza a girar con suerte chilena!",
+    "🎰🔥 Agüero Bet on Win Casino – donde la suerte se vuelve loca 🔥🎰 💸 ¿Creés en los milagros? Nosotros sí.",
+    "Descargá El Kun Casino y empezá con $3000 GRATIS + 300 giros de regalo 🎁. Pero ojo 👀... cada giro puede cambiar tu vida.",
+    "🌙 Jugá cuando quieras, donde quieras — y descubrí bonos secretos, premios sorpresa y torneos relámpago ⚡️ que aparecen sin aviso.",
+    "🎯 Soporte 24/7 con gente real (no bots, tranquilos 😎).",
+    "💎 Cada usuario nuevo tiene una chance de entrar al Modo Jackpot, donde los premios no tienen límite.",
+    "🎲 ¿Estás listo para probar si el destino está de tu lado?",
+    "👉 Descargá El Kun Casino ahora y hacé historia antes de que lo hagan tus amigos.",
   ],
 };
