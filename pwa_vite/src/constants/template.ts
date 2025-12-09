ec21ad514f1b27edb1f1a65afe4f143802edf471
developer-lumen <lumen@beton.win>
2025-12-09 15:06:14 +0200

    created new pwa

--- pwa_vite/src/constants/template.ts
+++ pwa_vite/src/constants/template.ts
@@ -4,28 +4,27 @@ import sliderImage1 from "../assets/slider_image_1.webp";
 import sliderImage2 from "../assets/slider_image_2.webp";
 import sliderImage3 from "../assets/slider_image_3.webp";
 import sliderImage4 from "../assets/slider_image_4.webp";
-// import sliderImage5 from "../assets/slider_image_5.webp";
+import sliderImage5 from "../assets/slider_image_5.webp";
+import sliderImage6 from "../assets/slider_image_6.webp";
 
 export const data = {
   productImage,
-  productName: "Agüero Casino",
-  productCreator: "BetonWin",
+  productName: "Boca Juniors Casino",
+  productCreator: "CABJ Ltd",
 
   images: [
     { src: sliderImage1, alt: "image 1" },
     { src: sliderImage2, alt: "image 2" },
     { src: sliderImage3, alt: "image 3" },
     { src: sliderImage4, alt: "image 4" },
-    // { src: sliderImage5, alt: "image 5" },
+    { src: sliderImage5, alt: "image 5" },
+    { src: sliderImage6, alt: "image 6" },
   ],
 
   description: [
-    "🎰🔥 Agüero Bet on Win Casino – donde la suerte se vuelve loca 🔥🎰 💸 ¿Creés en los milagros? Nosotros sí.",
-    "Descargá El Kun Casino y empezá con $3000 GRATIS + 300 giros de regalo 🎁. Pero ojo 👀... cada giro puede cambiar tu vida.",
-    "🌙 Jugá cuando quieras, donde quieras — y descubrí bonos secretos, premios sorpresa y torneos relámpago ⚡️ que aparecen sin aviso.",
-    "🎯 Soporte 24/7 con gente real (no bots, tranquilos 😎).",
-    "💎 Cada usuario nuevo tiene una chance de entrar al Modo Jackpot, donde los premios no tienen límite.",
-    "🎲 ¿Estás listo para probar si el destino está de tu lado?",
-    "👉 Descargá El Kun Casino ahora y hacé historia antes de que lo hagan tus amigos.",
+    "🎉💰Gana 3000$ al instante + 300 FS de bonificación 🎉💰 🏆",
+    "Presentamos nuestra nueva y emocionante aplicación de juegos 🎉 Sumérgete en un mundo de diversión con bonificaciones infinitas y sorpresas diarias 🎁. ¡Nadie se va con las manos vacías! 🏅",
+    "Únete a nuestra aplicación hoy mismo y prueba tu suerte en cualquier momento con asistencia las 24 horas del día, los 7 días de la semana 🕒.",
+    "Ya sea de día o de noche, ¡estamos listos para ayudarte a convertirte en un ganador! 🎯🌟",
   ],
 };
