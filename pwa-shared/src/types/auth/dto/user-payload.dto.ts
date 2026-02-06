732d97458edb78d2da59ebb47304dc678d7e0fa2
eliot-hash <eliot@beton.win>
2026-02-06 16:20:52 +0200

    fix

--- pwa-shared/src/types/auth/dto/user-payload.dto.ts
+++ pwa-shared/src/types/auth/dto/user-payload.dto.ts
@@ -1,5 +1,5 @@
 import {ScopeType} from "../../org/roles/enums/scope.enum";
-import {AccessLevel} from "../../../../../pwa-prisma/src";
+import {AccessLevel} from ".prisma/client";
 
 export type UserPayload = {
     id: string;
