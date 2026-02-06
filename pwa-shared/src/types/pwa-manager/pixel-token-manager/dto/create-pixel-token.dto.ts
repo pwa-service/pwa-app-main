0cb1c5ef86beea2a7ce0991347d4395adc48a101
eliot-hash <eliot@beton.win>
2026-02-06 05:50:51 +0200

    fix auth test

--- pwa-shared/src/types/pwa-manager/pixel-token-manager/dto/create-pixel-token.dto.ts
+++ pwa-shared/src/types/pwa-manager/pixel-token-manager/dto/create-pixel-token.dto.ts
@@ -2,17 +2,17 @@ import {ApiProperty, OmitType, PartialType} from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePixelTokenDto {
-    id: string;
+    id!: string;
 
     @ApiProperty({ example: 'EAA...' })
     @IsString()
     @IsNotEmpty()
-    token: string;
+    token!: string;
 
     @ApiProperty({ example: 'Основний піксель для FB', required: false })
     @IsOptional()
     @IsString()
     description?: string;
 
-    ownerId: string;
+    ownerId!: string;
 }
