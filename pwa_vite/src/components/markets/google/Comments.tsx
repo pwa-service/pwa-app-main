a1b8ec64e71d212f2603e6d9438dee1cc3cfc0c2
developer-lumen <lumen@beton.win>
2026-02-17 12:42:58 +0200

    added dynamic field from getPwaData

--- pwa_vite/src/components/markets/google/Comments.tsx
+++ pwa_vite/src/components/markets/google/Comments.tsx
@@ -1,34 +1,40 @@
-import type { GoogleCommentData } from "../../../types/market";
 import { memo } from "react";
 
 import Stars from "./Stars";
 
 interface CommentsProps {
-  comments: GoogleCommentData[];
+  comments: {
+    author: string;
+    text: string;
+    type: string;
+  }[];
 }
 
 const Comments = memo(({ comments }: CommentsProps) => {
   return (
     <div className="w-full flex flex-col gap-10 mt-16">
-      {comments.map(({ type, id, fullName, date, text }) =>
+      {comments.map(({ type, author, text }, index) =>
         type === "user" ? (
-          <div key={id} className="flex flex-col">
+          <div key={index} className="flex flex-col">
             <div className="flex items-center gap-4 mb-6">
-              <span className="font-medium text-zinc-800">{fullName}</span>
+              <span className="font-medium text-zinc-800">{author}</span>
             </div>
 
             <div className="flex items-center gap-2 mb-2">
               <Stars size={12} className="gap-0.5 text-[#3B8562]" />
-              <p className="text-xs text-zinc-600">{date}</p>
+              {/* <p className="text-xs text-zinc-600">{date}</p> */}
             </div>
 
             <p className="text-zinc-600 text-sm">{text}</p>
           </div>
         ) : (
-          <div key={id} className="w-full flex flex-col gap-2 p-4 -mt-4 rounded-lg bg-blue-100/30">
+          <div
+            key={index}
+            className="w-full flex flex-col gap-2 p-4 -mt-4 rounded-lg bg-blue-100/30"
+          >
             <div className="flex items-center gap-4 mb-2">
-              <span className="font-medium text-zinc-800">{fullName}</span>
-              <p className="text-xs text-zinc-600">{date}</p>
+              <span className="font-medium text-zinc-800">{author}</span>
+              <p className="text-xs text-zinc-600">{author}</p>
             </div>
 
             <p className="text-zinc-600 text-sm">{text}</p>
