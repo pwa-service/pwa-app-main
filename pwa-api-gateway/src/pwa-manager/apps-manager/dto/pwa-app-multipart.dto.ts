e4c1ac11b6c71695a255dbf3fc76ec50a24f7e60
eliot-hash <eliot@beton.win>
2026-02-23 01:41:54 +0200

    fix

--- pwa-api-gateway/src/pwa-manager/apps-manager/dto/pwa-app-multipart.dto.ts
+++ pwa-api-gateway/src/pwa-manager/apps-manager/dto/pwa-app-multipart.dto.ts
@@ -2,79 +2,80 @@ import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { PwaAppStatus } from '../../../../../pwa-shared/src/types/pwa-manager/apps-manager/enum/pwa-status.enum';
 
 export class CreateAppMultipartDto {
-    @ApiProperty()
+    @ApiProperty({ type: 'string' })
     name!: string;
 
-    @ApiProperty()
+    @ApiProperty({ type: 'string' })
     domainId!: string;
 
     @ApiProperty({
         enum: PwaAppStatus,
-        example: PwaAppStatus.DRAFT
+        example: PwaAppStatus.DRAFT,
+        type: 'string'
     })
     status!: PwaAppStatus;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     description?: string;
 
-    @ApiProperty({ example: '[{"author": "Admin", "text": "Comment"}]', description: 'JSON string of comments' })
+    @ApiProperty({ type: 'string', example: '[{"author": "Admin", "text": "Comment"}]', description: 'JSON string of comments' })
     comments!: string;
 
-    @ApiProperty({ example: 'en' })
+    @ApiProperty({ type: 'string', example: 'en' })
     lang!: string;
 
-    @ApiProperty({ example: '[{"text": "Terms..."}]', description: 'JSON string of terms' })
+    @ApiProperty({ type: 'string', example: '[{"text": "Terms..."}]', description: 'JSON string of terms' })
     terms!: string;
 
-    @ApiProperty({ example: '[{"text": "tag1"}]', description: 'JSON string of tags' })
+    @ApiProperty({ type: 'string', example: '[{"text": "tag1"}]', description: 'JSON string of tags' })
     tags!: string;
 
-    @ApiProperty({ example: '["page_view", "registration"]', description: 'JSON string of event types' })
+    @ApiProperty({ type: 'string', example: '["page_view", "registration"]', description: 'JSON string of event types' })
     events!: string;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     destinationUrl?: string;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     productUrl?: string;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     author?: string;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     rating?: string;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     adsText?: string;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     category?: string;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     categorySubtitle?: string;
 
     @ApiProperty({ required: false, type: 'number' })
     reviewsCount?: number;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     reviewsCountLabel?: string;
 
     @ApiProperty({ required: false, type: 'number' })
     appSize?: number;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     appSizeLabel?: string;
 
     @ApiProperty({ required: false, type: 'number' })
     installCount?: number;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     installCountLabel?: string;
 
     @ApiProperty({ required: false, type: 'number' })
     ageLimit?: number;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, type: 'string' })
     ageLimitLabel?: string;
 
     @ApiProperty({ type: 'string', format: 'binary', required: false, description: 'Icon image file' })
